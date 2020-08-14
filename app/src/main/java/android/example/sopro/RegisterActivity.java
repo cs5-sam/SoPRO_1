428443cc1c88229f2c7f9b790b7479996e8f096e
cs5-sam <54584505+cs5-sam@users.noreply.github.com>
2020-08-14 20:49:29 +0530

    initial commit

--- app/src/main/java/android/example/sopro/RegisterActivity.java
+++ app/src/main/java/android/example/sopro/RegisterActivity.java
@@ -2,6 +2,8 @@ package android.example.sopro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,9 +13,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,7 +34,10 @@ public class RegisterActivity extends AppCompatActivity {
     Button registerBtn;
     TextView loginText;
 
-    final String url_Register="";
+    final String BASE_URL = "http://node-dev.ap-southeast-1.elasticbeanstalk.com/";
+    final String REG_URL = BASE_URL + "registration";
+    private String reg_name, reg_password, reg_email;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +49,7 @@ public class RegisterActivity extends AppCompatActivity {
         passwordEdit = findViewById(R.id.password_input_register);
         registerBtn = findViewById(R.id.register_btn);
         loginText = findViewById(R.id.login_text);
+        builder = new AlertDialog.Builder(RegisterActivity.this);
 
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,67 +63,79 @@ public class RegisterActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameEdit.getText().toString();
-                String email = emailEdit.getText().toString();
-                String password = passwordEdit.getText().toString();
+                reg_name = nameEdit.getText().toString().trim();
+                reg_password = passwordEdit.getText().toString().trim();
+                reg_email = emailEdit.getText().toString().trim();
+                if(reg_name.equals("") || reg_email.equals("") || reg_password.equals("")){
+                    builder.setTitle("Something went wrong");
+                    builder.setMessage("Fill all the fields");
+                    displayAlert("Input error");
+                }
+                else{
+                    /*if(!(reg_password).equals(confirm_password)){
+                        builder.setTitle("Something went wrong");
+                        builder.setMessage("Passwords not match");
+                        displayAlert("Input error");
+                    }*/
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, REG_URL, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                /*JSONArray jsonArray = new JSONArray(response);
+                                JSONObject jsonObject = jsonArray.getJSONObject(0);
+                                String message = jsonObject.getString("message");*/
+                                String code = response;
 
-                new RegisterUser().execute(name,email,password);
-            }
-        });
-    }
+                                builder.setTitle("Server Response");
+                                builder.setMessage(code);
+                                displayAlert(code);
 
-    public class  RegisterUser extends AsyncTask<String,Void,String>{
-
-        @Override
-        protected String doInBackground(String... strings) {
-            String name = strings[0];
-            String email = strings[1];
-            String password = strings[2];
-            String finalUrl = url_Register + "?user_name=" + name +
-                    "&user_id=" + email +
-                    "&user_password=" + password;
-
-                try {
-                    OkHttpClient okHttpClient = new OkHttpClient();
-                    Request request = new Request.Builder()
-                            .url(finalUrl)
-                            .get()
-                            .build();
-                    Response response = null;
-
-                    try {
-                        response = okHttpClient.newCall(request).execute();
-                        if (response.isSuccessful()) {
-                            String result = response.body().string();
-
-                            if (result.equalsIgnoreCase("User registered successfully")) {
-                                showMessage("Register successful");
-                                Intent i = new Intent(RegisterActivity.this,
-                                        MainActivity.class);
-                                startActivity(i);
-                                finish();
-                            } else if (result.equalsIgnoreCase("User already exists")) {
-                                showMessage("User already exists");
-                            } else {
-                                showMessage("oop! please try again");
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            return null;
-        }
-    }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
 
-    public void showMessage(final String message){
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String,String> params = new HashMap<>();
+                            params.put("username",reg_name);
+                            params.put("password",reg_password);
+                            params.put("email",reg_email);
+                            return params;
+                        }
+                    };
+                    MySingleton.getInstance(RegisterActivity.this).addToRequestque(stringRequest);
+                }
+
             }
         });
+    }
+
+    public void displayAlert(final String message){
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(message.equals("Input error")){
+                    nameEdit.setText("");
+                    passwordEdit.setText("");
+                    emailEdit.setText("");
+                }
+                else if(message.equals("Registration SuccessFull " + reg_name)){
+                    finish();
+                }
+                else if(message.equals("Reg FAILED")){
+                    nameEdit.setText("");
+                    passwordEdit.setText("");
+                    emailEdit.setText("");
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
